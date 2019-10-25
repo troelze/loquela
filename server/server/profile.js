65494f4a576658d5cf3f013aa826facafaaf005b
laurenmackey <lauren.m.mackey@gmail.com>
2019-10-25 09:26:30 -0700

    Start microphone addition

--- server/server/profile.js
+++ server/server/profile.js
@@ -3,25 +3,26 @@ module.exports = function() {
     var router = express.Router();
     var db = require('../db/queries');
     var helpers = require('./helpers');
-    var session = require('express-session');
+    let {PythonShell} = require('python-shell');
+
+    // TODO: we'll need to get the ID based on who is logged in
+    var userId = 1;
 
     function getProfileData(userId) {
         return new Promise(function(resolve, reject) {
             var context = {};
-
+        
             db.getUserById(userId).then(function(userInfo) {
                 context.username = userInfo[0].username;
                 context.email = userInfo[0].email;
                 signupDate = new Date(userInfo[0].created_at);
-                formattedDate = (signupDate.getMonth() + 1) + '-' + signupDate.getDate() + '-' + signupDate.getFullYear();
+                formattedDate = (signupDate.getMonth() + 1) + '-' + signupDate.getDate() + '-' + signupDate.getFullYear()
                 context.signup = formattedDate;
-
+    
                 db.getUserProfileByUserId(userId).then(function(userProfileInfo) {
-                    if (userProfileInfo[0]) {
-                        context.language = helpers.capitalizeFirstLetter(userProfileInfo[0].language);
-                        context.difficulty = helpers.capitalizeFirstLetter(userProfileInfo[0].difficulty);
-                        context.topic = helpers.capitalizeFirstLetter(userProfileInfo[0].topic);
-                    }
+                    context.language = helpers.capitalizeFirstLetter(userProfileInfo[0].language);
+                    context.difficulty = helpers.capitalizeFirstLetter(userProfileInfo[0].difficulty);
+                    context.topic = helpers.capitalizeFirstLetter(userProfileInfo[0].topic);
                     context.imageUrl = helpers.getAvatarUrl(userId);
 
                     resolve(context);
@@ -39,12 +40,24 @@ module.exports = function() {
 
     router.get('/', function(req, res) {
         if(helpers.notLoggedIn(req)) {
-          res.render('login');
+            res.render('login');
         } else {
-          getProfileData(req.session.user.id).then(function(context) {
-              res.render('profile', context);
-          });
-      }
+            getProfileData(req.session.user.id).then(function(context) {
+                context.microphone = req.query.microphone ? 'icon-circle' : 'icon-microphone';
+                if (req.query.microphone) {
+                    // Source: https://medium.com/@HolmesLaurence/integrating-node-and-python-6b8454bfc272
+                    var options = {};
+                    PythonShell.run('../python/speech_input.py', options, function(err, data) {
+                        if (err) {
+                            console.log('Error:', err);
+                        } else {
+                            console.log('Data is:', data);
+                        }
+                    });
+                }
+                res.render('profile', context);
+            });
+        }
     });
 
     router.get('/edit', function(req, res) {
