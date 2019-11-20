0ae34e6850e258e27e2c5258d48acc4cdded8b05
Tres Oelze <34492927+troelze@users.noreply.github.com>
2019-11-20 09:50:52 -0700

    Update seeds.sql for testing purposes

--- db/migrations/seeds.sql
+++ db/migrations/seeds.sql
@@ -4,16 +4,49 @@ VALUES ('lauren', 'lauren@test.com', 'lauren'),
         ('hudson', 'hudson@test.com', 'hudson');
 
 
-INSERT INTO prompts (name, text, language, entities) 
-VALUES ('Prompt 1', 'Describe the weather today.', 'english', 'weather'),
-  ('Prompt 2', 'Describe your favorite movie', 'english', 'movies'),
-  ('Prompt 3', 'Describe your favorite book', 'english', 'books'),
-  ('Prompt 4', 'Describe your favorite area', 'english', 'areas'),
-  ('Prompt 5', 'Describe your favorite name', 'english', 'names'),
-  ('Prompt 6', 'Describe your favorite car', 'english', 'cars'),
-  ('Prompt 7', 'Describe your favorite place', 'english', 'place'),
-  ('Prompt 8', 'Describe your favorite person', 'english', 'person');
+INSERT INTO prompts (name, text, language, topic, entities) 
+VALUES ('Prompt 0', 'This is art prompt 0.', 'english', 'art', 'art'),
+  ('Prompt 1', 'This is art prompt 1.', 'english', 'art', 'art'),
+  ('Prompt 2', 'This is art prompt 2.', 'english', 'art', 'art'),
+  ('Prompt 3', 'This is art prompt 3.', 'english', 'art', 'art'),
+  ('Prompt 4', 'This is art prompt 4.', 'english', 'art', 'art'),
+  ('Prompt 5', 'This is art prompt 5.', 'english', 'art', 'art'),
+  ('Prompt 6', 'This is art prompt 6.', 'english', 'art', 'art'),
+  ('Prompt 7', 'This is art prompt 7.', 'english', 'art', 'art'),
+  ('Prompt 8', 'This is art prompt 8.', 'english', 'art', 'art'),
+  ('Prompt 9', 'This is art prompt 9.', 'english', 'art', 'art'),
+  ('Prompt 10', 'This is weather prompt 0.', 'english', 'weather', 'weather'),
+  ('Prompt 11', 'This is weather prompt 1.', 'english', 'weather', 'weather'),
+  ('Prompt 12', 'This is weather prompt 2.', 'english', 'weather', 'weather'),
+  ('Prompt 13', 'This is weather prompt 3.', 'english', 'weather', 'weather'),
+  ('Prompt 14', 'This is weather prompt 4.', 'english', 'weather', 'weather'),
+  ('Prompt 15', 'This is weather prompt 5.', 'english', 'weather', 'weather'),
+  ('Prompt 16', 'This is weather prompt 6.', 'english', 'weather', 'weather'),
+  ('Prompt 17', 'This is weather prompt 7.', 'english', 'weather', 'weather'),
+  ('Prompt 18', 'This is weather prompt 8.', 'english', 'weather', 'weather'),
+  ('Prompt 19', 'This is weather prompt 9.', 'english', 'weather', 'weather'),
+  ('Prompt 20', 'This is greetings prompt 0.', 'english', 'greetings', 'greetings'),
+  ('Prompt 21', 'This is greetings prompt 1.', 'english', 'greetings', 'greetings'),
+  ('Prompt 22', 'This is greetings prompt 2.', 'english', 'greetings', 'greetings'),
+  ('Prompt 23', 'This is greetings prompt 3.', 'english', 'greetings', 'greetings'),
+  ('Prompt 24', 'This is greetings prompt 4.', 'english', 'greetings', 'greetings'),
+  ('Prompt 25', 'This is greetings prompt 5.', 'english', 'greetings', 'greetings'),
+  ('Prompt 26', 'This is greetings prompt 6.', 'english', 'greetings', 'greetings'),
+  ('Prompt 27', 'This is greetings prompt 7.', 'english', 'greetings', 'greetings'),
+  ('Prompt 28', 'This is greetings prompt 8.', 'english', 'greetings', 'greetings'),
+  ('Prompt 29', 'This is greetings prompt 9.', 'english', 'greetings', 'greetings');
 
 
-INSERT INTO answers (prompt_id, correct_text) 
-VALUES (1, 'It is raining.');
+INSERT INTO prompt_activities (name, text, language, topic, entities) 
+VALUES ('Prompt 1', 'Describe the weather today.', 'english', 'art', 'art'),
+  ('Prompt 2', 'Describe your favorite A', 'english', 'art', 'art'),
+  ('Prompt 3', 'Describe your favorite B', 'english', 'art', 'art'),
+  ('Prompt 4', 'Describe your favorite C', 'english', 'weather', 'weather'),
+  ('Prompt 5', 'Describe your favorite D', 'english', 'weather', 'weather'),
+  ('Prompt 6', 'Describe your favorite E', 'english', 'weather', 'weather'),
+  ('Prompt 7', 'Describe your favorite F', 'english', 'sports', 'sports'),
+  ('Prompt 8', 'Describe your favorite G', 'english', 'sports', 'sports'),
+  ('Prompt 9', 'Describe your favorite H', 'english', 'sports', 'sports'),
+  ('Prompt 10', 'Describe your favorite I', 'english', 'greetings', 'greetings'),
+  ('Prompt 11', 'Describe your favorite J', 'english', 'greetings', 'greetings'),
+  ('Prompt 12', 'Describe your favorite K', 'english', 'greetings', 'greetings');
